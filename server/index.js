a207fc169e83a8c92d52a2301b948df9c8ee3215
ericpkatz <ericpkatz@gmail.com>
2023-06-01 11:06:41 -0400

    added webpack

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 const path = require('path');
 
 app.use('/public', express.static(path.join(__dirname, '../public')));
+app.use('/dist', express.static(path.join(__dirname, '../dist')));
 
 app.get('/', (req, res)=> {
   res.sendFile(path.join(__dirname, '../public/index.html'));
