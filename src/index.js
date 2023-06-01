9b1956c13ec4ba798e8ebd24175993d3d121c4fb
ericpkatz <ericpkatz@gmail.com>
2023-06-01 11:21:54 -0400

    setup react app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('hello world');
+import { createRoot } from 'react-dom/client';
+import React, { useState, useEffect } from 'react';
+const App = ()=> {
+  const [users, setUsers] = useState([]);
+  useEffect(()=> {
+    const fetchUsers = async()=> {
+      const response = await fetch('/api/users');
+      const json = await response.json();
+      setUsers(json);
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>The Martini Site!! ({ users.length })</h1>
+      <ul>
+        {
+          users.map( user => {
+            return (
+              <li key={ user.id }>
+                { user.username }
+              </li>
+            );
+          })
+        }
+      </ul>
+    </div>
+  );
+};
+
+const root = createRoot(document.querySelector('#root'));
+root.render(<App />);
+
