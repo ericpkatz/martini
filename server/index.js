fefff739a4cdae5030aa9a8385d9e0017c5bd6b6
ericpkatz <ericpkatz@gmail.com>
2023-06-01 10:43:13 -0400

    home route done

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
+
+app.use('/public', express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res)=> {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 app.listen(port, ()=> {
   console.log(`listening on port ${port}`);
